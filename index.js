aa8641c5bf0699a8283e7d6310efa32b99a4ce2a
Arnav Rangari <99029902+hashtag-longrangers@users.noreply.github.com>
2023-08-06 21:06:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Hi, I am Arnav Rangari", "A Web Developer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      color: "#EA1179",
+      sizeVariations: 2
+    });
+  };
